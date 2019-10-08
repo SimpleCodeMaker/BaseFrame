d9bd3adabc0e6d463c6dedfc9589f9165811467c
casual <123127611@qq.com>
2019-10-08 23:13:59 +0800

    通过反射获取当前activity栈

--- baseframe/src/main/java/com/casual/baseframe/utils/ActivityStackManager.kt
+++ baseframe/src/main/java/com/casual/baseframe/utils/ActivityStackManager.kt
@@ -1,8 +1,36 @@
 package com.casual.baseframe.utils
 
 import android.app.Activity
+import java.lang.reflect.AccessibleObject.setAccessible
+
+
 
 object ActivityStackManager {
     //目前先随便写个list
     val activityList = ArrayList<Activity>()
+
+    fun getAllActivitys(): List<Activity> {
+        val list = ArrayList<Activity>()
+        try {
+            val activityThread = Class.forName("android.app.ActivityThread")
+            val currentActivityThread = activityThread.getDeclaredMethod("currentActivityThread")
+            currentActivityThread.isAccessible = true
+            //获取主线程对象
+            val activityThreadObject = currentActivityThread.invoke(null)
+            val mActivitiesField = activityThread.getDeclaredField("mActivities")
+            mActivitiesField.isAccessible = true
+            val mActivities = mActivitiesField.get(activityThreadObject) as Map<Any, Any>
+            for ((_, value) in mActivities) {
+                val activityClientRecordClass = value.javaClass
+                val activityField = activityClientRecordClass.getDeclaredField("activity")
+                activityField.isAccessible = true
+                val o = activityField.get(value)
+                list.add(o as Activity)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return list
+    }
 }
